8ef7804a4648a51e6537bf3a94a2acb412610079
h <h@>
2018-12-24 08:28:58 +0800

    Add ttl vendor to youdao translator

---
+++ vendor/youdao/vendor/vendor.go
@@ -0,0 +1,31 @@
+// h 20181224
+//
+// Translator Vendor (Youdao)
+
+package vendor
+
+import (
+	"log"
+	"os"
+
+	. ".."
+	. "../../.."
+)
+
+// init
+// To import only
+func init() {
+	// KEY & SID
+	key, sid := os.Getenv("TTL_KEY_YOUDAO"), os.Getenv("TTL_SID_YOUDAO")
+	// Check $KEY and $SID
+	if key == "" || sid == "" {
+		// Log $KEY and $SID
+		log.Println("Nil $KEY/$SID")
+	}
+	// Register translator
+	Register(
+		// Name vendor
+		"youdao",
+		// Init translator
+		NewYoudaoTranslator(key, sid))
+}
