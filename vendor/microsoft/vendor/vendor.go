3f74dc5b232c22405601c4babf40f60d2af5daea
h <h@>
2018-12-27 09:09:08 +0800

    Update ttl vendor of microsoft translator

---
+++ vendor/microsoft/vendor/vendor.go
@@ -0,0 +1,31 @@
+// h 20181227
+//
+// Translator Vendor (Microsoft)
+
+package vendor
+
+import (
+	"log"
+	"os"
+
+	. ".."
+	. "../../.."
+)
+
+// init
+// To import only
+func init() {
+	// KEY & SID
+	key, sid := os.Getenv("TTL_KEY_MICROSOFT"), os.Getenv("TTL_SID_MICROSOFT")
+	// Check $KEY and $SID
+	if key == "" || sid == "" {
+		// Assert $KEY and $SID
+		log.Fatalln("Invalid $KEY/$SID")
+	}
+	// Register translator
+	Register(
+		// Name vendor
+		"microsoft",
+		// Init translator
+		NewMicrosoftTranslator(key, sid))
+}
